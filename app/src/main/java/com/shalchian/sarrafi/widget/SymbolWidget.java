5fa92bd5ac0b85a0cb4ae553d5ac7141dca8fd02
armin <armin536@gmail.com>
2020-10-04 13:20:25 +0330

    Added Refresher functions (Symbol Widget)

--- app/src/main/java/com/shalchian/sarrafi/widget/SymbolWidget.java
+++ app/src/main/java/com/shalchian/sarrafi/widget/SymbolWidget.java
@@ -18,12 +18,14 @@
 
 package com.shalchian.sarrafi.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
 
 import androidx.preference.PreferenceManager;
@@ -51,10 +53,20 @@ public class SymbolWidget extends AppWidgetProvider {
     // There may be multiple widgets active, so update all of them
     for (int appWidgetId : appWidgetIds) {
       RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.symbol_widget);
+
+      Intent refreshIntent = new Intent(context, SymbolWidget.class);
+      refreshIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+      refreshIntent.setAction("com.shalchian.sarrafi.widget.REFRESH");
+      PendingIntent refreshPendingIntent = PendingIntent.getBroadcast(context, 0, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+      views.setOnClickPendingIntent(R.id.symbol_refresh, refreshPendingIntent);
+
       if (ActivityHelper.checkConnection(context)) {
         requestData(context, views, appWidgetId, appWidgetManager);
       } else {
-        //TODO: ADD NOT CONNECTED EMPTY STATE
+        views.setViewVisibility(R.id.symbol_refresh, View.VISIBLE);
+        views.setImageViewBitmap(R.id.symbol_date, WidgetHelper.BuildUpdate(context.getResources().getString(R.string.no_network), "Shabnam-FD", (int) context.getResources().getDimension(R.dimen.symbol_details), context));
+        views.setViewVisibility(R.id.symbol_progress, View.GONE);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
       }
 
     }
@@ -81,13 +93,20 @@ public class SymbolWidget extends AppWidgetProvider {
       if (ActivityHelper.checkConnection(context)) {
         requestData(context, views, appWidgetId, appWidgetManager);
       } else {
-        //TODO: ADD NOT CONNECTED EMPTY STATE
+        views.setViewVisibility(R.id.symbol_refresh, View.VISIBLE);
+        views.setImageViewBitmap(R.id.symbol_date, WidgetHelper.BuildUpdate(context.getResources().getString(R.string.no_network), "Shabnam-FD", (int) context.getResources().getDimension(R.dimen.symbol_details), context));
+        views.setViewVisibility(R.id.symbol_progress, View.GONE);
+        appWidgetManager.updateAppWidget(appWidgetId, views);
       }
     }
   }
 
   private void requestData(Context context,RemoteViews views, int appWidgetId, AppWidgetManager appWidgetManager) {
     Log.e("♻️", "GETTING DATA");
+    views.setViewVisibility(R.id.symbol_refresh, View.GONE);
+    views.setImageViewBitmap(R.id.symbol_date, WidgetHelper.BuildUpdate(context.getResources().getString(R.string.updating), "Shabnam-FD", (int) context.getResources().getDimension(R.dimen.symbol_details), context));
+    views.setViewVisibility(R.id.symbol_progress, View.VISIBLE);
+    appWidgetManager.updateAppWidget(appWidgetId, views);
     AndroidNetworking
             .get("https://call.tgju.org/ajax.json")
             .setPriority(Priority.HIGH)
@@ -97,7 +116,6 @@ public class SymbolWidget extends AppWidgetProvider {
               @Override
               public void onResponse(JSONObject response) {
                 try {
-
                   SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
                   boolean tomanConvert = preferences.getBoolean("toman", false);
                   String iran_currency = tomanConvert ? "تومان" : "ریال";
@@ -126,16 +144,28 @@ public class SymbolWidget extends AppWidgetProvider {
                         views.setImageViewResource(R.id.symbol_percent_image, android.R.color.transparent);
                         break;
                     }
+
+                    views.setViewVisibility(R.id.symbol_refresh, View.VISIBLE);
+                    views.setViewVisibility(R.id.symbol_progress, View.GONE);
                     appWidgetManager.updateAppWidget(appWidgetId, views);
+                    Log.e("✅", "DONE DATA");
                   }
                 } catch (JSONException e) {
                   Log.e("🔴🔴 JSONException", String.valueOf(e));
+                  views.setViewVisibility(R.id.symbol_refresh, View.VISIBLE);
+                  views.setImageViewBitmap(R.id.symbol_date, WidgetHelper.BuildUpdate(context.getResources().getString(R.string.error_parsing), "Shabnam-FD", (int) context.getResources().getDimension(R.dimen.symbol_details), context));
+                  views.setViewVisibility(R.id.symbol_progress, View.GONE);
+                  appWidgetManager.updateAppWidget(appWidgetId, views);
                 }
 
               }
               @Override
               public void onError(ANError error) {
                 Log.e("🔴ERROR" , String.valueOf(error));
+                views.setViewVisibility(R.id.symbol_refresh, View.VISIBLE);
+                views.setImageViewBitmap(R.id.symbol_date, WidgetHelper.BuildUpdate(context.getResources().getString(R.string.error_loading), "Shabnam-FD", (int) context.getResources().getDimension(R.dimen.symbol_details), context));
+                views.setViewVisibility(R.id.symbol_progress, View.GONE);
+                appWidgetManager.updateAppWidget(appWidgetId, views);
               }
             });
   }
